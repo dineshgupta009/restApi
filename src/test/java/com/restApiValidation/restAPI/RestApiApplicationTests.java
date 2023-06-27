d2ad5d56fbafaf0cb972844f8fca0a16dece8afd
Dinesh Gupta <Dineshg0009@gmail.com>
2023-06-27 16:19:33 +0530

    second commit

---
+++ src/test/java/com/restApiValidation/restAPI/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restApiValidation.restAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
